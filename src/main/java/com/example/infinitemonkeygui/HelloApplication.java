a844094a71cd269cc6bc8ddff95cc452d3e3f1fa
kunwarm420 <78311053+kunwarm420@users.noreply.github.com>
2024-06-01 01:38:59 +0100

    First Commit
    first commit

---
+++ src/main/java/com/example/infinitemonkeygui/HelloApplication.java
@@ -0,0 +1,32 @@
+package com.example.infinitemonkeygui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+//    @Override
+//    public void start(Stage stage) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hello World");
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
